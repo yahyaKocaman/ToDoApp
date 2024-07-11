3b44c3f203b81675f8b45a45eaad21f00c5d556e
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 16:49:00 +0300

    Update BookViewModel.kt

--- app/src/main/java/com/t/todoapp/ui/BookViewModel.kt
+++ app/src/main/java/com/t/todoapp/ui/BookViewModel.kt
@@ -1,38 +1,37 @@
 package com.t.todoapp.ui
 
-import androidx.lifecycle.ViewModel // Android Architecture Components ViewModel sınıfı.
-import androidx.lifecycle.ViewModelProvider // ViewModel'ı oluşturmak için sağlayıcı sınıf.
-import androidx.lifecycle.viewModelScope // ViewModel kapsamını başlatmak için kullanılan kapsam işlevi.
-import com.t.todoapp.data.Book // Kitap veri modeli.
-import com.t.todoapp.data.BookRepository // Kitap verilerini yöneten depo sınıfı.
-import kotlinx.coroutines.launch /* Asenkron işlemleri yönetmek için kullanılan coroutines bileşeni.
-                                                            uzun süren işlemleri başlatmak için kullanırız*/
+import androidx.lifecycle.ViewModel 
+import androidx.lifecycle.ViewModelProvider 
+import androidx.lifecycle.viewModelScope 
+import com.t.todoapp.data.Book
+import com.t.todoapp.data.BookRepository 
+import kotlinx.coroutines.launch
+                                                            
 
-// Kitap verileriyle etkileşimi yöneten ViewModel sınıfı.
+
 class BookViewModel(private val repository: BookRepository) : ViewModel() {
 
-    // Tüm kitapları gösteren LiveData.
+   
     val allBooks = repository.allBooks
 
-    // Bir kitabı ekleme işlevi.
+  
     fun insert(book: Book) = viewModelScope.launch {
         repository.insert(book)
     }
 
-    // Bir kitabı silme işlevi.
+   
     fun delete(book: Book) = viewModelScope.launch {
         repository.delete(book)
     }
 
-    // Bir kitabı güncelleme işlevi.
     fun update(book: Book, numDay: Any?) = viewModelScope.launch {
         repository.update(book, numDay.toString())
     }
 }
 
-// ViewModel'ı oluşturmak için fabrika sınıfı.
+
 class BookViewModelFactory(private val repository: BookRepository) : ViewModelProvider.Factory {
-    // ViewModel oluşturma işlevi.
+   
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(BookViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
