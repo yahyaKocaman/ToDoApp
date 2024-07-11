0f3fce17ceab6029b34f58bcddbe23d993388d6c
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 16:49:53 +0300

    Update NavGraph.kt

--- app/src/main/java/com/t/todoapp/ui/NavGraph.kt
+++ app/src/main/java/com/t/todoapp/ui/NavGraph.kt
@@ -9,29 +9,25 @@ import androidx.navigation.compose.composable
 import com.t.todoapp.ui.screens.AddBookScreen
 import com.t.todoapp.ui.screens.BookListScreen
 
-/**
- * Uygulamanın gezinme grafiğini oluşturan ana bileşen.
- * Farklı rotalara sahip ekranları belirler.
- */
 @Composable
 fun NavGraph(navController: NavHostController, viewModel: BookViewModel) {
-    // Jetpack Navigation ile belirli rotalara sahip ekranlar arasında gezinme sağlanır
+
     NavHost(navController = navController, startDestination = "bookList") {
-        // Kitap listesi ekranı
+      
         composable("bookList") {
-            // Kitap listesi bileşeni
+            
             BookListScreen(navController = navController, viewModel = viewModel)
         }
-        // Yeni kitap ekleme ekranı
+  
         composable("addBook") {
-            // Yeni kitap ekleme bileşeni
+          
             AddBookScreen(navController = navController, viewModel = viewModel)
         }
-        // Kitap istatistikleri ekranı
+      
         composable("bookStatistics") {
-            // Kitap istatistikleri bileşeni
+           
             BookStatisticsScreen(
-                // ViewModel'den alınan kitap listesi verisi kullanılarak istatistikler görüntülenir
+         
                 books = viewModel.allBooks.collectAsState(initial = emptyList()).value,
                 navController = navController
             )
