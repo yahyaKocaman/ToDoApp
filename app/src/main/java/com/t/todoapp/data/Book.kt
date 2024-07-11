632dd108648ace723a93ff524319fecfb4369488
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 16:52:16 +0300

    Update Book.kt

--- app/src/main/java/com/t/todoapp/data/Book.kt
+++ app/src/main/java/com/t/todoapp/data/Book.kt
@@ -1,14 +1,14 @@
-package com.t.todoapp.data // Kodun hangi pakette olduğunu belirtiyor.
+package com.t.todoapp.data 
 
-import androidx.room.Entity // Room veritabanı için gerekli.
-import androidx.room.PrimaryKey // Birincil anahtar için gerekli.
+import androidx.room.Entity 
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "books") // Bu sınıfı "books" tablosu olarak tanımlıyor.
+@Entity(tableName = "books") 
 data class Book(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0, // Otomatik artan ID, birincil anahtar.
-    val title: String, // Kitabın başlığı.
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String, 
     val author: String,
     val numPage: String,
     val numDay: String,
-    val isRead: Boolean = false, // Kitap okundu mu? (Varsayılan: hayır).
+    val isRead: Boolean = false, 
 )
