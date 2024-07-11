e3c546174512d5751a539bea38fda6ef281a0b26
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 16:54:58 +0300

    Update BookRepository.kt

--- app/src/main/java/com/t/todoapp/data/BookRepository.kt
+++ app/src/main/java/com/t/todoapp/data/BookRepository.kt
@@ -1,28 +1,28 @@
-package com.t.todoapp.data // Kodun hangi pakette olduğunu belirtiyor.
+package com.t.todoapp.data 
 
-import kotlinx.coroutines.flow.Flow // Flow kütüphanesi.
-import kotlinx.coroutines.flow.map // Flow içindeki map fonksiyonu.
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map 
 
-class BookRepository(private val bookDao: BookDao) { // BookDao'yu kullanan depo sınıfı.
+class BookRepository(private val bookDao: BookDao) {
 
-    val allBooks: Flow<List<Book>> = bookDao.getAllBooks().map { books -> // Tüm kitapları alır ve her seferinde map ile işleyip yazdırır.
+    val allBooks: Flow<List<Book>> = bookDao.getAllBooks().map { books -> 
         books.also {
-            println("Fetched books: $it") // Alınan kitapları yazdırır.
+            println("Fetched books: $it") 
         }
     }
 
-    suspend fun insert(book: Book) { // Kitap ekleme fonksiyonu.
-        bookDao.insertBook(book) // Kitabı DAO'ya ekler.
-        println("Inserted book: $book") // Eklenen kitabı yazdırır.
+    suspend fun insert(book: Book) {
+        bookDao.insertBook(book) 
+        println("Inserted book: $book")
     }
 
-    suspend fun delete(book: Book) { // Kitap silme fonksiyonu.
-        bookDao.deleteBook(book) // Kitabı DAO'dan siler.
-        println("Deleted book: $book") // Silinen kitabı yazdırır.
+    suspend fun delete(book: Book) { 
+        bookDao.deleteBook(book) 
+        println("Deleted book: $book") 
     }
 
-    suspend fun update(book: Book, numDay: String) { // Kitap güncelleme fonksiyonu, numDay parametresi eklenmiş.
-        bookDao.updateBook(book.copy(numDay = numDay)) // Kitabın numDay alanını günceller.
-        println("Updated book: $book") // Güncellenen kitabı yazdırır.
+    suspend fun update(book: Book, numDay: String) { 
+        bookDao.updateBook(book.copy(numDay = numDay))
+        println("Updated book: $book") 
     }
 }
