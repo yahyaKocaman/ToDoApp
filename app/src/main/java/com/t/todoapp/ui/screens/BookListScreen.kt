95a90f1423b2505396813b7e7923a57560bd6cbe
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 17:04:20 +0300

    Update BookListScreen.kt

--- app/src/main/java/com/t/todoapp/ui/screens/BookListScreen.kt
+++ app/src/main/java/com/t/todoapp/ui/screens/BookListScreen.kt
@@ -1,33 +1,33 @@
 package com.t.todoapp.ui.screens
 
-import androidx.compose.foundation.Image // Resim bileşeni için.
-import androidx.compose.foundation.background // Arka plan rengi eklemek için.
-import androidx.compose.foundation.layout.* // Layout düzenlemeleri için.
-import androidx.compose.material3.* // Material 3 bileşenleri için.
-import androidx.compose.runtime.Composable // Composable fonksiyonlar için.
-import androidx.compose.runtime.collectAsState // Flow'ları state olarak toplamak için.
-import androidx.compose.runtime.getValue // State değerlerini almak için.
-import androidx.compose.ui.Modifier // Modifikasyonlar için.
-import androidx.compose.ui.graphics.Color // Renkler için.
-import androidx.compose.ui.layout.ContentScale // İçerik ölçeklendirme için.
-import androidx.compose.ui.res.painterResource // Resim kaynakları için.
-import androidx.compose.ui.unit.dp // Birim ölçüleri için.
-import androidx.navigation.NavHostController // Navigasyon için.
-import com.t.todoapp.ui.BookViewModel // ViewModel sınıfı.
-import com.t.todoapp.ui.components.BookList // BookList bileşeni.
-import androidx.compose.material.icons.Icons // Material ikonlar için.
-import androidx.compose.material.icons.filled.Add // Add ikonu için.
-import androidx.compose.material.icons.filled.Check // Check ikonu için.
-import androidx.compose.ui.Alignment // Hizalama için.
-import androidx.compose.ui.draw.scale // Ölçekleme için.
-import androidx.compose.ui.text.font.FontFamily // Yazı tipi ailesi için.
-import androidx.compose.ui.text.font.FontStyle // Yazı tipi stili için.
-import com.t.todoapp.R // Kaynaklar için.
+import androidx.compose.foundation.Image 
+import androidx.compose.foundation.background 
+import androidx.compose.foundation.layout.* 
+import androidx.compose.material3.* 
+import androidx.compose.runtime.Composable 
+import androidx.compose.runtime.collectAsState 
+import androidx.compose.runtime.getValue 
+import androidx.compose.ui.Modifier 
+import androidx.compose.ui.graphics.Color 
+import androidx.compose.ui.layout.ContentScale 
+import androidx.compose.ui.res.painterResource 
+import androidx.compose.ui.unit.dp 
+import androidx.navigation.NavHostController 
+import com.t.todoapp.ui.BookViewModel
+import com.t.todoapp.ui.components.BookList 
+import androidx.compose.material.icons.Icons 
+import androidx.compose.material.icons.filled.Add 
+import androidx.compose.material.icons.filled.Check 
+import androidx.compose.ui.Alignment 
+import androidx.compose.ui.draw.scale 
+import androidx.compose.ui.text.font.FontFamily 
+import androidx.compose.ui.text.font.FontStyle 
+import com.t.todoapp.R 
 
-@OptIn(ExperimentalMaterial3Api::class) // Deneysel Material 3 API'lerini kullanmak için.
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookListScreen(navController: NavHostController, viewModel: BookViewModel) {
-    val books by viewModel.allBooks.collectAsState(initial = emptyList()) // Kitap listesini state olarak toplar.
+    val books by viewModel.allBooks.collectAsState(initial = emptyList()) 
 
     Scaffold(
         topBar = {
@@ -36,8 +36,8 @@ fun BookListScreen(navController: NavHostController, viewModel: BookViewModel) {
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(Color.White.copy(alpha = 0.4f, red = 0.1f, green = 0.1f, blue = 0.1f)), // Arka plan rengini ayarlar.
-                        contentAlignment = Alignment.Center // İçeriği ortalar.
+                            .background(Color.White.copy(alpha = 0.4f, red = 0.1f, green = 0.1f, blue = 0.1f)), 
+                        contentAlignment = Alignment.Center 
                     ) {
                         Text(
                             text = "Book List",
@@ -50,49 +50,49 @@ fun BookListScreen(navController: NavHostController, viewModel: BookViewModel) {
                     }
                 },
                 colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = Color.White.copy(alpha = 0.4f, red = 0.1f, green = 0.1f, blue = 0.1f) // Üst çubuğun arka plan rengini ayarlar.
+                    containerColor = Color.White.copy(alpha = 0.4f, red = 0.1f, green = 0.1f, blue = 0.1f)
                 )
             )
         },
         floatingActionButton = {
             Column {
                 FloatingActionButton(onClick = {
-                    navController.navigate("addBook") // "addBook" ekranına gider.
+                    navController.navigate("addBook") 
                 }) {
-                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add Book") // Add ikonunu gösterir.
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add Book") 
                 }
-                Spacer(modifier = Modifier.height(16.dp)) // İkonlar arası boşluk bırakır.
+                Spacer(modifier = Modifier.height(16.dp))
                 FloatingActionButton(onClick = {
-                    navController.navigate("bookStatistics") // "bookStatistics" ekranına gider.
+                    navController.navigate("bookStatistics") 
                 }) {
-                    Icon(imageVector = Icons.Default.Check, contentDescription = "Book Statistics") // Check ikonunu gösterir.
+                    Icon(imageVector = Icons.Default.Check, contentDescription = "Book Statistics") 
                 }
             }
         }
     ) { paddingValues ->
-        // BookListScreen içinde BookItem çağrısı güncellendi
+       
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues) // Padding değerlerini uygular.
+                .padding(paddingValues)
         ) {
             Image(
                 modifier = Modifier
                     .fillMaxSize()
-                    .scale(1f), // Resmi 1x ölçekler.
-                painter = painterResource(id = R.drawable.kutuphane), // Arka plan resmini yükler.
+                    .scale(1f),
+                painter = painterResource(id = R.drawable.kutuphane), 
                 contentDescription = "Background image",
-                contentScale = ContentScale.Crop // İçeriği kırparak ölçekler.
+                contentScale = ContentScale.Crop 
             )
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(16.dp) // 16dp iç padding uygular.
+                    .padding(16.dp) 
             ) {
                 BookList(
-                    books = books, // Kitap listesi.
-                    onUpdate = { book, numDay -> viewModel.update(book, numDay) }, // Kitap güncelleme fonksiyonu.
-                    onDelete = { book -> viewModel.delete(book) } // Kitap silme fonksiyonu.
+                    books = books, 
+                    onUpdate = { book, numDay -> viewModel.update(book, numDay) }, 
+                    onDelete = { book -> viewModel.delete(book) } 
                 )
             }
         }
