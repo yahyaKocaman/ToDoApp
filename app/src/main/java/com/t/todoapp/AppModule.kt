a4990779c9620dd109fbdf547031557aa74eaaf9
Yahya Kocaman <120945684+yahyaKocaman@users.noreply.github.com>
2024-07-11 16:47:45 +0300

    Update AppModule.kt

--- app/src/main/java/com/t/todoapp/AppModule.kt
+++ app/src/main/java/com/t/todoapp/AppModule.kt
@@ -6,43 +6,31 @@ import com.t.todoapp.data.BookDatabase
 import com.t.todoapp.data.BookRepository
 import com.t.todoapp.ui.BookViewModelFactory
 
-/**
- * Uygulama modülü, veritabanı ve repository gibi bağımlılıkları yönetir.
- */
 object AppModule {
     private var database: BookDatabase? = null
 
-    /**
-     * Veritabanı erişimini sağlar.
-     * Eğer bir veritabanı oluşturulmadıysa, yeni bir tane oluşturur.
-     */
+
     private fun provideDatabase(context: Context): BookDatabase {
         return database ?: synchronized(this) {
             val instance = Room.databaseBuilder(
-                context.applicationContext, // Uygulama bağlamı için bir bağlam nesnesi alır
-                BookDatabase::class.java, // Veritabanı sınıfını belirtir
-                "book_database" // Veritabanı adı
-            ).build() // Veritabanı oluşturur
-            database = instance // Veritabanı örneğini saklar
-            instance // Veritabanı örneğini döndürür
+                context.applicationContext, 
+                BookDatabase::class.java,
+                "book_database" 
+            ).build() 
+            database = instance 
+            instance
         }
     }
 
-    /**
-     * Repository sağlar.
-     * Veritabanı ve DAO kullanarak bir repository oluşturur.
-     */
+ 
     private fun provideRepository(context: Context): BookRepository {
-        val database = provideDatabase(context) // Veritabanı örneğini alır
-        return BookRepository(database.bookDao()) // BookRepository örneği oluşturur ve döndürür
+        val database = provideDatabase(context)
+        return BookRepository(database.bookDao()) 
     }
 
-    /**
-     * ViewModelFactory sağlar.
-     * Repository kullanarak bir ViewModelFactory oluşturur.
-     */
+ 
     fun provideViewModelFactory(context: Context): BookViewModelFactory {
-        val repository = provideRepository(context) // Repository örneğini alır
-        return BookViewModelFactory(repository) // ViewModelFactory örneği oluşturur ve döndürür
+        val repository = provideRepository(context)
+        return BookViewModelFactory(repository) 
     }
 }
